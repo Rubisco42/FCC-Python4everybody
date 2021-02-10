8b6266acc0b67daa1c2925996009df10117f4597
Rubisco42 <74238637+Rubisco42@users.noreply.github.com>
2021-02-11 00:37:54 +0100

    dodo

--- 02 - Expressions B.py
+++ 02 - Expressions B.py
@@ -1,3 +1,13 @@
 #operators ** = power (exposant) % =remainder (reste d'une division)
 #results of a division is always a float
+#operator precedence rule (math rresolution order):
+#parenthese
+#exponentiation
+#multiplication, division and remainder (left to right if no par)
+#addition and substraction
+#left to right
+
+#type() function indicates type of the variable
+
+#stoped at 7:40 min
 
