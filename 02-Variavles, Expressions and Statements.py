fa0bd5eabbb3df4616b69aa2f856aef828e305c3
Rubisco42 <74238637+Rubisco42@users.noreply.github.com>
2021-02-10 23:43:41 +0100

    pasbon

---
+++ 02-Variavles, Expressions and Statements.py
@@ -0,0 +1,21 @@
+x1q3z9ocd = 35.0
+x1q3z9afd = 12.50
+x1q3p9afd = x1q3z9ocd * x1q3z9afd
+print(x1q3p9afd)
+#inconvenient and long variable names
+
+hours = 35.0
+rate = 12.50
+pay = hours * rate
+print (pay)
+#mnemonic variable names, convenient but be carefull not to reuse variable name => overwrite
+
+x = 2
+x = x + 2
+print (x)
+# because x ends up containing a function ( x=x+2), print (x) is called "function call"
+# when an expression ("x + 2") is assigned to an existing variable with a  given value,
+# Pyhton first evaluates the expression with the assigned value and then updates variable value in consequence
+
+
+
