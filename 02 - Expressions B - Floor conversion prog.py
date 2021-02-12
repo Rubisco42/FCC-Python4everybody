663ec7eb7da6210c7bf3a16a4cd6170aeeb651c0
Rubisco42 <74238637+Rubisco42@users.noreply.github.com>
2021-02-12 14:51:12 +0100

    upgraded and loop added

--- 02 - Expressions B - Floor conversion prog.py
+++ 02 - Expressions B - Floor conversion prog.py
@@ -1,14 +1,38 @@
-ctry = input("In which country are you")
-if ctry == "USA":
-    inp1 = input("Europe floor?")
-    usf = int(inp1) + 1
-    print("US Floor", usf)
-if ctry == "Europe":
-    inp2 = input("US Floor?")
-    euf = int(inp2) - 1
-    if inp2 == "0":
-        euf = "RDC"
-    print("Europe floor", euf)
-else:
-    print("good luck mofo")
+def prgm():
+    ctry = input("In which country are you? ")
+    if ctry == "USA":
+        inp1 = input("Europe floor? ")
+        usf = int(inp1) + 1
+        print("US Floor", usf)
+    else:
+        print("good luck")
+    if ctry == "Europe":
+        inp2 = input("US Floor? ")
+        euf = int(inp2) - 1
+        if inp2 == "0":
+            euf = "RDC"
+        print("Europe floor", euf)
+    else:
+        print("good luck")
+
+
+name = input("Who are you? ")
+ext = input ("Do you want to determine floor equivalence? ")
+while ext:
+    if ext == "no":
+        print("good bye")
+        break
+    if ext == "yes":
+        prgm()
+        tex = input ("Again? ")
+        if tex != "yes":
+            print("See you soon!", name)
+            break
+
+
+
+
+
+
+
 
