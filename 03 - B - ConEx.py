80740294f0fbf23aa4f25d4925553e7095ebc387
Rubisco42 <74238637+Rubisco42@users.noreply.github.com>
2021-02-17 23:33:28 +0100

    more

--- 03 - B - ConEx.py
+++ 03 - B - ConEx.py
@@ -11,4 +11,9 @@ else:
     print("large")
 
 #elif can be added infinitely
+#do not overlap condition like:
+# if x < 2:
+#elif x <20
+#elif x < 10  : 6 is below 10 and below 20. second elif will never run
+#be careful to the order of of conditional statement
 
